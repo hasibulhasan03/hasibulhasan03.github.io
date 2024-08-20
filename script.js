5ae0777ed85c18b905f6636640b3d8ff729f836b
Md Hasibul Hasan <hello.hasibulhasan03@gmail.com>
2024-08-20 16:43:53 +0600

    shortcut disabled

--- script.js
+++ script.js
@@ -48,3 +48,33 @@ document.addEventListener("DOMContentLoaded", function() {
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('currentYear').textContent = new Date().getFullYear();
 });
+
+
+document.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+});
+
+document.addEventListener('keydown', function(e) {
+
+    if (e.ctrlKey && (e.key === 'u' || e.key === 'U')) {
+        e.preventDefault();
+    }
+
+    if (e.key === 'F12') {
+        e.preventDefault();
+    }
+
+    if (e.ctrlKey && e.shiftKey && (e.key === 'i' || e.key === 'I')) {
+        e.preventDefault();
+    }
+
+    if (e.ctrlKey && e.shiftKey && (e.key === 'j' || e.key === 'J')) {
+        e.preventDefault();
+    }
+
+    if (e.ctrlKey && e.shiftKey && (e.key === 'c' || e.key === 'C')) {
+        e.preventDefault();
+    }
+});
+
+
