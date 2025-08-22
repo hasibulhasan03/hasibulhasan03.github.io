1718159bd99e5a21792b9f85132a90c4ce209e30
Md Hasibul Hasan <hello.hasibulhasan03@gmail.com>
2025-08-22 12:52:30 +0600

    updated

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
     var typed = new Typed('#typed', {
-        strings: ["Tech Enthusiast", "Developer", "Programmer"],
+        strings: ["Tech Enthusiast", "Network Engineer", "Developer", "Programmer"],
         typeSpeed: 150,
         backSpeed: 100,
         loop: true
     });
 });
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const navLinks = document.querySelectorAll("nav a");
 
-    navLinks.forEach(function(link) {
-        link.addEventListener("click", function(event) {
+    navLinks.forEach(function (link) {
+        link.addEventListener("click", function (event) {
             const currentActive = document.querySelector(".active");
             if (currentActive) {
                 currentActive.classList.remove("active");
@@ -22,18 +22,18 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const tabButtons = document.querySelectorAll(".tab-btn");
     const projectContents = document.querySelectorAll(".project-content");
 
-    tabButtons.forEach(function(button) {
-        button.addEventListener("click", function(event) {
+    tabButtons.forEach(function (button) {
+        button.addEventListener("click", function (event) {
             const tabName = button.getAttribute("data-tab");
 
-            tabButtons.forEach(function(btn) {
+            tabButtons.forEach(function (btn) {
                 btn.classList.remove("active");
             });
-            projectContents.forEach(function(content) {
+            projectContents.forEach(function (content) {
                 content.classList.remove("active");
             });
 
@@ -45,16 +45,16 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('currentYear').textContent = new Date().getFullYear();
 });
 
 
-document.addEventListener('contextmenu', function(e) {
+document.addEventListener('contextmenu', function (e) {
     e.preventDefault();
 });
 
-document.addEventListener('keydown', function(e) {
+document.addEventListener('keydown', function (e) {
 
     if (e.ctrlKey && (e.key === 'u' || e.key === 'U')) {
         e.preventDefault();
